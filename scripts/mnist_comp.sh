c0154722d328606a3751eb00e7a09ce4e846cbc4
gitiauxx <xavier.gitiaux@gmail.com>
2021-05-04 18:09:06 -0400

    Add Binarized MNIST on gpu

---
+++ scripts/mnist_comp.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#SBATCH --job-name=mnist_comp
+#SBATCH --output=/scratch/xgitiaux/mnist_comp_%j.out
+#SBATCH --error=/scratch/xgitiaux/mnist_comp_%j.error
+#SBATCH --mail-user=xgitiaux@gmu.edu
+#SBATCH --mail-type=END
+#SBATCH --export=ALL
+#SBATCH --partition=gpuq
+#SBATCH --nodes 1
+#SBATCH --tasks 1
+#SBATCH --mem=64G
+#SBATCH --qos=csqos
+#SBATCH --gres=gpu:1
+
+module load python/3.6.7
+module load cuda/10.2
+source ../fvae-env/bin/activate
+
+echo $SLURM_ARRAY_TASK_ID
+../fvae-env/bin/python3 eval.py --config_path configs/mnist/mnist_pareto_comp_cnn.yml --seed 0 --beta 1.0 --tmax 1.0
