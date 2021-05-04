fdd610394fe4cb69dc6c3eb2145e3358299fc891
gitiauxx <xavier.gitiaux@gmail.com>
2021-05-04 16:36:40 -0400

    Dimension of rec loss is 3 not 4

--- source/losses/discmixlogistic_loss.py
+++ source/losses/discmixlogistic_loss.py
@@ -16,13 +16,13 @@ class DiscMixLogisticLoss(TemplateLoss):
     def forward(self, target, output):
         """
 
-        :param output: reconstructed input (B, C, W, H)
+        :param output: reconstructed input (B, M * 6 + M, W, H)
         :param target: initial input (B, C, W, H)
         :return: mean squared loss
         """
         num_mix = int(output.shape[1] / 7)
         dist = DiscMixLogistic(output, num_mix=num_mix)
         rec_loss = - dist.log_prob(target)
-        rec_loss = torch.mean(rec_loss.sum(dim=[1, 2, 3]))
+        rec_loss = torch.mean(rec_loss.sum(dim=[1, 2]))
 
         return rec_loss
