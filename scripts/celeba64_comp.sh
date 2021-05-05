7076cfc10e7f2357e482b5c00a77d747b3705367
gitiauxx <xavier.gitiaux@gmail.com>
2021-05-05 13:27:44 -0400

    Larger model for celeba

--- scripts/celeba64_comp.sh
+++ scripts/celeba64_comp.sh
@@ -11,11 +11,11 @@
 #SBATCH --tasks 1
 #SBATCH --mem=64G
 #SBATCH --qos=csqos
-#SBATCH --gres=gpu:4
+#SBATCH --gres=gpu:6
 
 module load python/3.6.7
 module load cuda/10.2
 source ../fvae-env/bin/activate
 
 echo $SLURM_ARRAY_TASK_ID
-../fvae-env/bin/python3 eval.py --config_path configs/celeba64/celeba_pareto_comp_cnn.yml --seed 0 --beta 1.0 --tmax 1.0
+../fvae-env/bin/python3 eval.py --config_path configs/celeba64/celeba_pareto_comp_cnn.yml --seed 0 --beta 0.0 --tmax 1.0
