fb5f5559a1e5d5278946315ff73de38a8dd4b7c8
gitiauxx <xavier.gitiaux@gmail.com>
2021-05-17 19:11:44 -0400

    Test

--- scripts/cifar_comp.sh
+++ scripts/cifar_comp.sh
@@ -11,8 +11,8 @@
 #SBATCH --tasks 1
 #SBATCH --mem=64G
 #SBATCH --qos=csqos
-#SBATCH --gres=gpu:2
-#SBATCH -t 0-5:00
+#SBATCH --gres=gpu:4
+#SBATCH -t 0-10:00
 
 module load python/3.6.7
 module load cuda/10.2
