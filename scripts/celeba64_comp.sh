bb9703fc93ff2099c3d06867854afe7784e2c7cf
gitiauxx <xavier.gitiaux@gmail.com>
2021-05-19 12:31:56 -0400

    Test

--- scripts/celeba64_comp.sh
+++ scripts/celeba64_comp.sh
@@ -14,9 +14,9 @@
 #SBATCH --qos=csqos
 #SBATCH --gres=gpu:4
 
-module load python/3.6.7
+module load python/3.8.4
 module load cuda/10.2
-source ../fvae-env/bin/activate
+source ../afbc-env/bin/activate
 
 echo $SLURM_ARRAY_TASK_ID
 ../fvae-env/bin/python3 eval.py --config_path configs/celeba64/celeba_pareto_comp_cnn.yml --seed 0 --beta 0.0 --tmax 1.0
