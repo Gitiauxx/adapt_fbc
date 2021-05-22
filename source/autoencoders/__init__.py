af780d08dd70b12c549d61378ad6013afb8f2840
gitiauxx <xavier.gitiaux@gmail.com>
2021-05-22 15:07:16 -0400

    Test

--- source/autoencoders/__init__.py
+++ source/autoencoders/__init__.py
@@ -1,4 +1,5 @@
 from source.autoencoders.fc_gated import FCGated
 from source.autoencoders.cnn_gated import CNNGated
+from source.autoencoders.cnn_quant import CNNQuant
 
-__all__ = ['FCGated', 'CNNGated']
+__all__ = ['FCGated', 'CNNGated', 'CNNQuant']
