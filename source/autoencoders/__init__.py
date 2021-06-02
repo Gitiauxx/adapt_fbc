02b0c78672d05d3478d4f5986c03b519b9e52cdf
gitiauxx <xavier.gitiaux@gmail.com>
2021-06-02 11:53:17 -0400

    VQVAe

--- source/autoencoders/__init__.py
+++ source/autoencoders/__init__.py
@@ -1,5 +1,6 @@
 from source.autoencoders.fc_gated import FCGated
 from source.autoencoders.cnn_gated import CNNGated
 from source.autoencoders.cnn_quant import CNNQuant
+from source.autoencoders.vqvae import VQVAE
 
-__all__ = ['FCGated', 'CNNGated', 'CNNQuant']
+__all__ = ['FCGated', 'CNNGated', 'CNNQuant', 'VQVAE']
