6b570fd57e67bb3c81ae1d422a5e04de2c976519
gitiauxx <xavier.gitiaux@gmail.com>
2021-06-12 13:46:31 -0400

    VQVAe

--- train_vqvae.py
+++ train_vqvae.py
@@ -160,7 +160,7 @@ if __name__ == "__main__":
     )
     parser.add_argument("--dist_url", default=f"tcp://127.0.0.1:{port}")
 
-    parser.add_argument("--size", type=int, default=256)
+    parser.add_argument("--size", type=int, default=64)
     parser.add_argument("--epoch", type=int, default=560)
     parser.add_argument("--lr", type=float, default=3e-4)
     #parser.add_argument("--sched", type=str)
