56c6e93b76fb0c0f3462e78fe7e75b0739a23b54
gitiauxx <xavier.gitiaux@gmail.com>
2021-06-13 10:05:46 -0400

    Dist

--- celeba_dist.sh
+++ celeba_dist.sh
@@ -17,23 +17,23 @@
 
 ### change 5-digit MASTER_PORT as you wish, slurm will raise Error if duplicated with others
 ### change WORLD_SIZE as gpus/node * num_nodes
-export MASTER_PORT=12340
-export WORLD_SIZE=3
-
-### get the first node name as master address - customized for vgg slurm
-### e.g. master(gnodee[2-5],gnoded1) == gnodee2
-echo "NODELIST="${SLURM_NODELIST}
-
-if [ ${SLURM_NODELIST:7:1} == "," ]; then
-    echo "MASTER_ADDR="${SLURM_NODELIST:0:7}
-    export MASTER_ADDR=${SLURM_NODELIST:0:7}
-elif [ ${SLURM_NODELIST:6:1} == "[" ]; then
-    echo "MASTER_ADDR="${SLURM_NODELIST:0:6}${SLURM_NODELIST:7:1}
-    export MASTER_ADDR=${SLURM_NODELIST:0:6}${SLURM_NODELIST:7:1}
-else
-    echo "MASTER_ADDR="${SLURM_NODELIST}
-    export MASTER_ADDR=${SLURM_NODELIST}
-fi
+#export MASTER_PORT=12340
+#export WORLD_SIZE=3
+#
+#### get the first node name as master address - customized for vgg slurm
+#### e.g. master(gnodee[2-5],gnoded1) == gnodee2
+#echo "NODELIST="${SLURM_NODELIST}
+#
+#if [ ${SLURM_NODELIST:7:1} == "," ]; then
+#    echo "MASTER_ADDR="${SLURM_NODELIST:0:7}
+#    export MASTER_ADDR=${SLURM_NODELIST:0:7}
+#elif [ ${SLURM_NODELIST:6:1} == "[" ]; then
+#    echo "MASTER_ADDR="${SLURM_NODELIST:0:6}${SLURM_NODELIST:7:1}
+#    export MASTER_ADDR=${SLURM_NODELIST:0:6}${SLURM_NODELIST:7:1}
+#else
+#    echo "MASTER_ADDR="${SLURM_NODELIST}
+#    export MASTER_ADDR=${SLURM_NODELIST}
+#fi
 
 module load python/3.8.4
 module load cuda/10.2
