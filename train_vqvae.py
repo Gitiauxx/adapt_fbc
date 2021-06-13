575ecb764d48089d7c2778947025223210184c02
gitiauxx <xavier.gitiaux@gmail.com>
2021-06-12 22:22:45 -0400

    VQVAe

--- train_vqvae.py
+++ train_vqvae.py
@@ -29,7 +29,7 @@ def train(epoch, loader, model, optimizer, scheduler, device, entropy_coder, pop
     acc_sum = 0
 
     latent_loss_weight = 0.25 * 100000
-    beta = min(10**(-1) * (1 + epoch), 1.0)
+    beta = 1.0
 
     for i, data in enumerate(loader):
         img = data['input']
