cd1559784835e868fa64be172152ea7409d5357a
gitiauxx <xavier.gitiaux@gmail.com>
2021-08-01 13:59:14 -0400

    Fix loading

--- train_vqvae.py
+++ train_vqvae.py
@@ -54,7 +54,7 @@ def train(epoch, loader, model, optimizer, scheduler, device, entropy_coder, ent
     acc_sum = 0
 
     latent_loss_weight = 0.25 * 100000
-    beta = 1.0
+    beta = 10
 
     for i, data in enumerate(loader):
         img = data[0]
